b18d448041a98c86811c9820b4ee6e7dad360587
Hannah Graham <91922724+hannah-r-graham@users.noreply.github.com>
2025-05-28 09:56:29 -0700

    fix prod API base url

--- src/azureBlobService.js
+++ src/azureBlobService.js
@@ -1,5 +1,5 @@
 
-const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'dogbreedbackend-c3fahebfdgefcwf8.eastus2-01.azurewebsites.net';
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'https://dogbreedbackend-c3fahebfdgefcwf8.eastus2-01.azurewebsites.net';
 
 export async function uploadImage(file) {
   const formData = new FormData();
